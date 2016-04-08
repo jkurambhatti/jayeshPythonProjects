189db1d7a0420243b70a253bf9b7e59420e8805f
jayesh <jkurambhatti2992@gmail.com>
2016-04-08 16:25:14 +0530

    dictionary sort using zip, lambda

--- PythonExperiments/dictionary.py
+++ PythonExperiments/dictionary.py
@@ -15,6 +15,32 @@ print menu_card     # prints all k - v pairs
 print menu_card['lassi']    # 50
 
 for k, v in menu_card.items():  # another way to access key value pairs
-    print k , v
+    print k, v
 
-    
+
+# to find the min, max and sort the dictionary items
+
+print menu_card.values()    # print all values from key:value pair
+print  menu_card.keys()     # print all keys from key:value pair
+
+print min(menu_card.values())   # 50
+print max(menu_card.values())   # 250
+
+print sorted(menu_card.values())    # print all sorted values from key:value pair
+
+# a function called zip() is used to zip 2 lists together
+# the only thing to remember is that what ever list you pass first only that shall be acted upon
+# so if u want to sort accord. to values then zip values list first : eg :
+
+
+
+menu_card_zipped = zip(menu_card.values(), menu_card.keys())
+print min(menu_card_zipped) # prints the value and key associated together
+# output : (50, 'lassi')
+
+print max(menu_card_zipped)
+#ouptput : (250, 'mushroom masala')
+
+print sorted(menu_card_zipped)
+# output : (50, 'lassi'), (120, 'dal tadka'), (150, 'veg kolhapuri'), (180, 'kaju curry'), (200, 'panner butter masala'), (250, 'mushroom masala')]
+
---
+++ PythonExperiments/lambda.py
@@ -0,0 +1,13 @@
+'''
+    now we are going to lean about "lambda" function in python
+    a lambda function is a small nameless fn and is called only once and never again
+    any function which you want to use just once and name is not important should be declared as lambda
+    example :
+    declare a function which returns square of the number passed
+    applications : see later
+
+'''
+
+
+result = lambda x: x * x
+print result(5)
